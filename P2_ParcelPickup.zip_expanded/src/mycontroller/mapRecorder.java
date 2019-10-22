73427763f0d4d930ea0998caa717f033facbb4b1
Uni-qunzhiw <55023955+Uni-qunzhiw@users.noreply.github.com>
2019-10-22 17:06:57 +1100

    Update mapRecorder.java

--- P2_ParcelPickup.zip_expanded/src/mycontroller/mapRecorder.java
+++ P2_ParcelPickup.zip_expanded/src/mycontroller/mapRecorder.java
@@ -1,13 +1,15 @@
 package mycontroller;
 import world.World;
 import java.util.HashMap;
+import java.util.Map;
+
 import tiles.MapTile;
 import utilities.Coordinate;
 
 public class mapRecorder {
 	
 	public enum TileType {
-		WALL, ROAD, PARCEL, START, EXIT
+		WALL, ROAD, PARCEL, START, EXIT, UNREACHABLE
 	}
 	
 	public MapTile[][] map;
@@ -18,6 +20,21 @@ public class mapRecorder {
 	
 	public mapRecorder(HashMap<Coordinate, MapTile> m_hashMap) {
 		// TODO Auto-generated constructor stub
+		map = new MapTile[MAP_WIDTH][MAP_HEIGHT];
+		tileType = new TileType[MAP_WIDTH][MAP_HEIGHT];
+		
+		
+		
+	}
+	
+	
+	public void CarView(HashMap<Coordinate, MapTile> Carview) {
+		
+		for(Map.Entry<Coordinate, MapTile> entry : Carview.entrySet()) {
+			int carX = entry.getKey().x;
+			int carY = entry.getKey().y;
+			
+		}
 	}
 
 }
