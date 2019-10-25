6451ff79bdeb00bcde43a0353eeaa94f43ee11a2
Uni-qunzhiw <55023955+Uni-qunzhiw@users.noreply.github.com>
2019-10-25 18:10:54 +1100

    Update MapRecorder.java

--- P2_ParcelPickup.zip_expanded/src/mycontroller/MapRecorder.java
+++ P2_ParcelPickup.zip_expanded/src/mycontroller/MapRecorder.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import java.util.Random;
 
 import tiles.MapTile;
+import tiles.ParcelTrap;
+
 import utilities.Coordinate;
 import java.util.ArrayList;
 
@@ -19,6 +21,7 @@ public class MapRecorder {
 	public MapTile[][] map;
 	public TileType[][] tileType;
 	public ArrayList<Coordinate> list; // unvisited coordinates list
+	public ArrayList<Coordinate> ParcelList;
 	public Coordinate coordinate;
 	public Random rand;
 
@@ -27,60 +30,37 @@ public class MapRecorder {
 
 	public MapRecorder(HashMap<Coordinate, MapTile> m_hashMap) {
 		// TODO Auto-generated constructor stub
-		maze = new int[MAP_WIDTH+50][MAP_HEIGHT+50];
-		Coordinate[][] map_coord = new Coordinate[MAP_WIDTH+50][MAP_HEIGHT+50];
-		map = new MapTile[MAP_WIDTH+50][MAP_HEIGHT+50];
-		tileType = new TileType[MAP_WIDTH+50][MAP_HEIGHT+50];
+		maze = new int[MAP_WIDTH][MAP_HEIGHT];
+		map = new MapTile[MAP_WIDTH][MAP_HEIGHT];
+		tileType = new TileType[MAP_WIDTH][MAP_HEIGHT];
 		list = new ArrayList<Coordinate>();
+		ParcelList = new ArrayList<Coordinate>();
 
 		System.out.println("Instantiating MapRecorder");
-		System.out.println("_______________________________MAP________________________________");
+
 		for (Map.Entry<Coordinate, MapTile> entry : m_hashMap.entrySet()) {
 			int i = entry.getKey().x;
 			int j = entry.getKey().y;
-			//System.out.print("("+String.format("%4s", entry.getKey())+")");
-			map[j][ i] = entry.getValue();
-			map_coord[j][i] = new Coordinate(j,i);
-			if(map[j][i].isType(MapTile.Type.WALL)) {
-				tileType[j][i] = TileType.WALL;	
-			}
-			
-//			System.out.println(String.format("%4s", i + "," +j+ entry.getValue().getType()));
-		}
-		for (int i =0; i < MAP_HEIGHT; i++) {
-			for (int j = 0; j <MAP_WIDTH; j++) {
-				System.out.print(String.format("%4s", map[i][j].getType()));
-				System.out.print(" ");
-			}
-			System.out.print("\n");
-			
-		}
-		System.out.println("________________________________________MAP____________________________________");
-		System.out.print("\n");
-		System.out.println("_________________________MAP-COORDINATE_____________________________");
-		for (int i =0; i < MAP_HEIGHT; i++) {
-			for (int j = 0; j <MAP_HEIGHT; j++) {
-				System.out.print(String.format("%4s", map_coord[i][j].toString()));
-				System.out.print(" ");
-			}
-			System.out.print("\n");
-		}
-		System.out.println("________________________________MAP-COORDINATE__________________________");
-		System.out.print("\n");
-//		for (int i = 0; i < MAP_WIDTH; i++) {
-//			for (int j = 0; j<MAP_HEIGHT; j++) {
-//				//System.out.println(i+","+j);
-//				if(tileType[i][j] == null && !map[i][j].isType(MapTile.Type.WALL)) {
-//					tileType[i][j] = TileType.UNSEARCHED;
-//					addToList(i, j, list);
-//				}
-//			}
-//		}
 
-		for (int i =0; i < MAP_HEIGHT; i++) {
-			for (int j = 0; j <MAP_WIDTH; j++) {
+			map[i][j] = entry.getValue();
+
+			if(map[i][j].isType(MapTile.Type.WALL)) {
+				tileType[i][j] = TileType.WALL;
+			}
+		}
+
+		for (int i = 0; i < MAP_WIDTH; i++) {
+			for (int j = 0; j<MAP_HEIGHT; j++) {
+				if(tileType[i][j] == null) {
+					tileType[i][j] = TileType.UNSEARCHED;
+					addToList(i, j, list);
+				}
+			}
+		}
+
+		for (int j = MAP_HEIGHT - 1; j>=0; j--) {
+			for (int i = 0; i< MAP_WIDTH; i++) {
 				//System.out.print(map[i][j].getType());
-				
 				if(map[i][j].isType(MapTile.Type.WALL)) {
 					maze[i][j] = -1;
 					//System.out.print(MAP_HEIGHT);
@@ -89,37 +69,19 @@ public class MapRecorder {
 					maze[i][j] = 0;
 				}
 				//System.out.print(maze[i][j]);
-//				System.out.print(String.format("%4s", maze[i][j]));
-//				System.out.print(" ");
-			}
-//			System.out.print("\n");
-		}
-		System.out.println("_____________________MAZE________________");
-		for (int i =0; i < MAP_HEIGHT; i++) {
-			for (int j = 0; j <MAP_WIDTH; j++) {
-//				System.out.print(String.format("%4s", map[i][j].getType()));
 				System.out.print(String.format("%4s", maze[i][j]));
 				System.out.print(" ");
 			}
 			System.out.print("\n");
 		}
-		System.out.println("_____________________MAZE________________");
 		
-		System.out.println("_____________________TILETYPE________________");
-		for (int i =0; i < MAP_HEIGHT; i++) {
-			for (int j = 0; j <MAP_WIDTH; j++) {
-//				System.out.print(String.format("%4s", map[i][j].getType()));
-				System.out.print(String.format("%4s", tileType[i][j]));
-				System.out.print(" ");
-			}
-			System.out.print("\n");
-		}
-		System.out.println("_____________________TILETYPE________________");
 
+	
 	}
 
 
 
+	
 
 	public void CarView(HashMap<Coordinate, MapTile> Carview) {
 
@@ -129,15 +91,27 @@ public class MapRecorder {
 
 			if (!TileOutOfRange(carX, carY) && tileType[carX][carY] == TileType.UNSEARCHED) {
 				tileType[carX][carY] = TileType.SEARCHED;
-				if(map[carX][carY].getType() == MapTile.Type.TRAP) {
+				
+				if(entry.getValue().isType(MapTile.Type.TRAP)) {
+					System.out.println("FOUND PARCEL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 					tileType[carX][carY] = TileType.PARCEL;
-				}
+					addToParcelList(carX,carY,ParcelList);
+				} 
 				DeleteFromList(carX, carY, list);
 			}
 
 		}
 	}
 
+	public void addToParcelList(int x, int y, ArrayList<Coordinate> ParcelList) {
+		ParcelList.add(new Coordinate(x, y));
+		System.out.println(ParcelList.get(0).x);
+	}
+
+	public void DeleteFromParcelList(int x, int y, ArrayList<Coordinate> ParcelList) {
+		ParcelList.remove(new Coordinate(x,y));
+	}
+	
 	public void addToList(int x, int y, ArrayList<Coordinate> list) {
 		list.add(new Coordinate(x, y));
 	}
@@ -148,7 +122,6 @@ public class MapRecorder {
 
 	public static Coordinate randomItem(ArrayList<Coordinate> mylist) {
 	    Random rand = new Random();
-	    System.out.print("myLISTSIZE= " + mylist.size());
 	    Coordinate randomCoord = mylist.get(rand.nextInt(mylist.size()));
 	    return randomCoord;
 	}
